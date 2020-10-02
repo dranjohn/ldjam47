b347e519feaf52bdf6d41557f5fbe7e529a0638d
dranjohn <dransheld@outlook.de>
2020-10-03 01:13:37 +0200

    Add rotating background image

--- js/main.js
+++ js/main.js
@@ -6,48 +6,18 @@ var canvas, ctx;
 var width, height;
 var keyboard;
 
-var img, snd;
-var camera;
+var backgroundImage;
 
 function update(now) {
 	keyboard.update();
 
-	if (keyboard.keys.ArrowDown.pressed) {
-		console.log(now);
-		snd.play();
-	}
-
 	ctx.setTransform(1, 0, 0, 1, 0, 0);
 	ctx.clearRect(0, 0, width, height);
 
-	//camera.transform();
+	ctx.translate(384, -128);
+	ctx.rotate(Math.PI * now / 1000.0);
 
-	ctx.scale(height/10, height/10);
-	ctx.translate((width/2)/(height/10), (height/2)/(height/10));
-	ctx.rotate(Math.PI/2);
-	ctx.translate(-(width/2)/(height/10), -(height/2)/(height/10));
-	ctx.translate(1, 0);
-	//ctx.translate(10, 10);
-	//ctx.rotate(Math.PI/4);
-	//ctx.translate(-10, -10);
-	//ctx.translate(20, 0);
-
-	/*ctx.setTransform(10, 0, 0, -10, 400, 240);
-
-	ctx.strokeStyle = "white";
-	ctx.moveTo(0, 0);
-	ctx.lineTo(10, 20);
-	ctx.stroke();
-
-	ctx.fillStyle = "green";
-	ctx.fillRect(0, 0, -2, -2);
-
-	ctx.fillStyle = "blue";
-	ctx.fillRect(-3, -3, -2, -2);
-	ctx.strokeStyle = "brown";
-	ctx.strokeRect(-3, -3, -2, -2);*/
-
-	ctx.drawImage(img, 8, 8, 1, 2);
+	ctx.drawImage(backgroundImage, -384, -384, 768, 768);
 
 	requestAnimationFrame(update);
 }
@@ -64,9 +34,7 @@ function main() {
 	addEventListener("keydown", e => keyboard.keydown(e));
 	addEventListener("keyup", e => keyboard.keyup(e));
 
-	img = new SrcImage("images/player3.png");
-	snd = new Audio("sound/test.wav");
-	camera = new Camera(ctx, 0, 0, 1, 0);
+	backgroundImage = new SrcImage("images/test_picture.png");
 
 	requestAnimationFrame(update);
 }
